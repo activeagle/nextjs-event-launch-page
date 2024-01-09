21baafca29327822b530c90c0728c5ee55b257b2
activeagle <activespin7@gmail.com>
2024-01-09 11:34:19 +0700

    [💡]: Updates: CountDown Timer

---
+++ src/components/countDownTimer.tsx
@@ -0,0 +1,43 @@
+import { calculateTimeToEvent } from "@/utils/countDownUtils";
+import { type Framework } from "@/utils/frameworkUtils";
+import { useState, useEffect } from "react";
+import { TimeUnit } from "./timeUnits";
+
+export const countDownTimer = ({ 
+    currentFramework, 
+} : { 
+    currentFramework: Framework
+}) => {
+    const [countdown, setCountdown] = useState(calculateTimeToEvent());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCountdown(CalculateTimeToEvent())
+        }, 1000)
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return <div className="flex gap-[10px]">
+        <TimeUnit
+            label="DAYS"
+            value={countdown.days}
+            currentFramework={currentFramework}
+        />
+         <TimeUnit
+            label="Hours"
+            value={countdown.hours}
+            currentFramework={currentFramework}
+        />
+         <TimeUnit
+            label="MINUTES"
+            value={countdown.minutes}
+            currentFramework={currentFramework}
+        />
+         <TimeUnit
+            label="SECONDS"
+            value={countdown.seconds}
+            currentFramework={currentFramework}
+        />
+    </div>
+};
---
+++ src/components/timeUnits.tsx
@@ -0,0 +1,32 @@
+import { Framework } from "@/utils/frameworkUtils";
+import { cn } from "@/utils/tailwindUtils";
+import { NumberRotation } from "./numberRotation";
+
+export const TimeUnit = ({
+    label, value, currentFramework
+}: {
+    label: string;
+    value: number;
+    currentFramework: Framework
+}) => {
+    return (
+    <div className="flex flex-col">
+        <div className="text-white text-3xl font-semibold">{value}</div>
+            <NumberRotation number={value}/>
+            <div className={cn("text-[8px] font-medium",
+            {
+                "text-sky-300": currentFramework === "safari",
+                "text-yellow-300": currentFramework === "chrome",
+                "text-teal-300": currentFramework === "tailwind",
+                "text-blue-300": currentFramework === "react",
+                "text-green-300": currentFramework === "vue",
+                "text-orange-300": currentFramework === "svelte",
+                "text-red-300": currentFramework === "mobile",
+                "text-neutral-300": currentFramework === "desktop",
+            })}
+            >
+                {label}
+            </div>
+    </div>
+    );
+};
